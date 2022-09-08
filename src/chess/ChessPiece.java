0a35a57812589ae89bf0c917f81676ff06996bed
jsmarques98 <jsmarques2011@gmail.com>
2022-09-08 02:23:43 +0100

    Check logic

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -17,6 +17,10 @@ public abstract class ChessPiece extends Piece {
         return color;
     }
 
+    public ChessPosition getChessPosition() {
+        return ChessPosition.fromPosition(position);  //o metodo fromPosition serve para passar a posicao para uma posicao do jogo
+    }
+
     protected boolean isThereOpponentPiece(Position position) {
         ChessPiece p = (ChessPiece) getBoard().piece(position);
         return p != null && p.getColor() != color;
